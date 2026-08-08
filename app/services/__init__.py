c582f4b639c85b22e70975a595a7936ff7b8832b
Santygo_83@rediffmail.com <santygo_83@rediffmail.com>
2026-08-08 19:06:30 +0530

    Replace repository contents

--- app/services/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Service layer: all business logic lives here, isolated from routes."""
--- app/utils/timeutil.py
+++
@@ -1,7 +0,0 @@
-"""Small time utilities."""
-from datetime import UTC, datetime
-
-
-def utcnow() -> datetime:
-    """Timezone-naive UTC now (stored naive for DB portability)."""
-    return datetime.now(UTC).replace(tzinfo=None)
